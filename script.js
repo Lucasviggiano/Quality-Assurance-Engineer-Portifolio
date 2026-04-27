395242c82a642395fbebd7091db4c61c8b10d149
Quality Engineer (QE) <168561927+Lucasviggiano@users.noreply.github.com>
2026-04-27 20:31:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const revealElements = document.querySelectorAll(".reveal");
 
   const observer = new IntersectionObserver(
